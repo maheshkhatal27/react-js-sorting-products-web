5e8778bcf662efb63fc8c88d4b739e38aaf129c5
maheshkhatal27 <mbkhatal@gmail.como>
2022-10-28 13:02:28 +0000

    sorting feature add

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,106 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
+
+import ProductCard from '../ProductCard'
+import ProductsHeader from '../ProductsHeader'
+import './index.css'
+
+const sortbyOptions = [
+  {
+    optionId: 'PRICE_HIGH',
+    displayText: 'Price (High-Low)',
+  },
+  {
+    optionId: 'PRICE_LOW',
+    displayText: 'Price (Low-High)',
+  },
+]
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: false,
+    activeOptionId: sortbyOptions[0].optionId,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const {activeOptionId} = this.state
+    this.setState({
+      isLoading: true,
+    })
+    const jwtToken = Cookies.get('jwt_token')
+    // const apiUrl = 'https://apis.ccbp.in/products'
+    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({
+        productsList: updatedData,
+        isLoading: false,
+      })
+    }
+  }
+
+  updateActiveOptionId = activeOptionId => {
+    // this.setState({activeOptionId: activeOptionId})
+    // since both the id passsed and state name is same we weill get shorthand error so write as follows
+    // this.setState({activeOptionId}) -removing this for new way of dealing
+    /* componenetDidMount is called whenever threre is a state change,but here we want getProducts details 
+to give us sorted products list hence we are calling the getProducts here i.e. whenever a dropdown has been 
+selected */
+    this.getProducts() // see more correct way below
+    /* the setState() is asynchronoums, it takes an optional callback parameter that can be used to make updates 
+    after the state is changed */
+    this.setState({activeOptionId}, this.getProducts)
+  }
+
+  renderProductsList = () => {
+    const {productsList, activeOptionId} = this.state
+    return (
+      <>
+        <ProductsHeader
+          sortbyOptions={sortbyOptions}
+          activeOptionId={activeOptionId}
+          updateActiveOptionId={this.updateActiveOptionId}
+        />
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  renderLoader = () => (
+    <div className="products-loader-container">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+    return isLoading ? this.renderLoader() : this.renderProductsList()
+  }
+}
+
+export default AllProductsSection
